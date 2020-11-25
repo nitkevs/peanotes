6c4be3b973054ddedfd3bea0002cd73b2a57d968
nikevs <nitkevs@gmail.com>
2020-11-25 21:15:49 +0200

    Создал предварительный дизайн главной страницы

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Заметки</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <header>
+      <h1>Заметки</h1>
+      <nav id="header-navigation">
+        <a href="">Добавить заметку</a>
+        <a href="">Справка</a>
+      </nav>
+    </header>
+    <main>
+      <div id="notes-list">
+        Здесь ещё ничего ещё нет.<br><a href="">Добавить заметку</a>
+      </div>
+      <div id="note-content">
+        Содержимое
+      </div>
+    </main>
+  </body>
