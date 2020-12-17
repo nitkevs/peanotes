008a4438315bab66bba7f2d10259f3ece0101e17
nikevs <nitkevs@gmail.com>
2020-12-17 14:59:36 +0200

    Немного изменил форматирование includes/DB_tables.php

--- includes/DB_tables.php
+++ includes/DB_tables.php
@@ -2,5 +2,6 @@
 
   /* Создаём таблицу pn_notes, если она ещё не существует */
 
- $query = "CREATE TABLE IF NOT EXISTS `pn_notes` (id int(8) PRIMARY KEY AUTO_INCREMENT, `title` text, `content` text, `timestamp` varchar(10) NOT NULL UNIQUE, `last_modified` varchar(10))";
-  $result = mysqli_query($db_connection, $query);
+$query = "CREATE TABLE IF NOT EXISTS `pn_notes` (id int(8) PRIMARY KEY AUTO_INCREMENT, `title` text, `content` text, `timestamp` varchar(10) NOT NULL UNIQUE, `last_modified` varchar(10))";
+
+$result = mysqli_query($db_connection, $query);
