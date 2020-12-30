d7841069501006ee4bc5401c37effbb28fcb6a6e
nikevs <nitkevs@gmail.com>
2020-12-30 18:54:58 +0200

    Добавлены обратные кавычки в названиях некоторых полей в DB_tables.php

--- includes/DB_tables.php
+++ includes/DB_tables.php
@@ -2,12 +2,12 @@
 
   /* Создаём таблицу pn_notes, если она ещё не существует */
 
-$query = "CREATE TABLE IF NOT EXISTS `pn_notes` (id int(8) PRIMARY KEY AUTO_INCREMENT, `title` text, `content` text, `timestamp` varchar(10) NOT NULL UNIQUE, `last_modified` varchar(10))";
+$query = "CREATE TABLE IF NOT EXISTS `pn_notes` (`id` int(8) PRIMARY KEY AUTO_INCREMENT, `title` text, `content` text, `timestamp` varchar(10) NOT NULL UNIQUE, `last_modified` varchar(10))";
 
 $result = mysqli_query($db_connection, $query);
 
 
-$query = "CREATE TABLE IF NOT EXISTS `pn_users` (id int(8) PRIMARY KEY AUTO_INCREMENT, `name` varchar(32), `login` varchar(32), `pass` char(32), `salt` char(32), `group` tinyint(1), `ban_expires` varchar(10), `ban_severity` tinyint(1)";
+$query = "CREATE TABLE IF NOT EXISTS `pn_users` (`id` int(8) PRIMARY KEY AUTO_INCREMENT, `name` varchar(32), `login` varchar(32), `pass` char(32), `salt` char(32), `group` tinyint(1), `ban_expires` varchar(10), `ban_severity` tinyint(1)";
 
 $result = mysqli_query($db_connection, $query);
 
