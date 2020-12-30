f1aeeeb089bdeb4ba22eda2febf81fd4c9f27ba0
nikevs <nitkevs@gmail.com>
2020-12-30 18:33:02 +0200

    В таблицу pn_users добавлены поля ban_expires и ban_severity
    • ban_expires будет содержать дату окончания запрета авторизации,
    либо будет пустым, если запрета нет.
    • ban_severity будет содержать информацию о строгости запрета,
    числа от 0 до 4.
    • В случае ввода неправильного пароля вступает в силу запрет большей
    строгости. Устанавливается новый бан на более длительный период.
    Например:
    0 - запрета нет;
    1 - таймаут авторизации 5 секунд (для ботов это существенная задежка);
    2 - таймаут авторизации 30 секунд;
    3 - таймаут авторизации 5 минут;
    4 - таймаут авторизации 30 минут.

--- includes/DB_tables.php
+++ includes/DB_tables.php
@@ -7,6 +7,6 @@ $query = "CREATE TABLE IF NOT EXISTS `pn_notes` (id int(8) PRIMARY KEY AUTO_INCR
 $result = mysqli_query($db_connection, $query);
 
 
-$query = "CREATE TABLE IF NOT EXISTS `pn_users` (id int(8) PRIMARY KEY AUTO_INCREMENT, `name` varchar(32), `login` varchar(32), `hash` char(32), `salt` char(32), `group` tinyint(1))";
+$query = "CREATE TABLE IF NOT EXISTS `pn_users` (id int(8) PRIMARY KEY AUTO_INCREMENT, `name` varchar(32), `login` varchar(32), `hash` char(32), `salt` char(32), `group` tinyint(1), `ban_expires` varchar(10), `ban_severity` tinyint(1)";
 
 $result = mysqli_query($db_connection, $query);
