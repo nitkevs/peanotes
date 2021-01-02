49dffba31959bb3231ad506e39dd5ee15ed1baaf
nikevs <nitkevs@gmail.com>
2021-01-02 20:05:16 +0200

    Приведены в порядок файлы login.php, auth.php, style.css
    Вынес стили из login.php в style.css.
    Привёл код login.php, auth.php, style.css в порядок.
    Исправлены мелкие недочёты.

--- includes/auth.php
+++ includes/auth.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+*
+* auth.php
+*
+* Скрипт авторизации пользователей
+* Реализует авторизацию по логину и паролю,
+* либо по куки-файлам.
+*
+*/
+
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -10,80 +20,82 @@ if (!session_id()) {
 
 require_once "{$_SERVER['DOCUMENT_ROOT']}/includes/DB_connection.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/includes/DB_tables.php";
-require_once "{$_SERVER['DOCUMENT_ROOT']}/includes/classes/User.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/includes/key.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/includes/global_functions.php";
 
-    // Создать хэш сессии
-    // Создать сессию
-    // Создать куку на 30 дней
-    // Перейти на главную
+function remove_session($session_hash) {
+  global $db_connection;
 
-  function set_session($user_id) {
-    global $user_agent_hash;
-    global $db_connection;
+  setcookie('session', '', time());
 
-    $random_hash = get_hash(rand(0, PHP_INT_MAX), rand(0, PHP_INT_MAX), rand(0, PHP_INT_MAX));
-    $coockie_hash = get_hash($random_hash, $user_id, $user_agent_hash);
-    $session_expires = time() + (60*60*24*30);
-    $new_session_hash = get_hash($coockie_hash, $user_agent_hash, HASH_KEY);
+  $query = "DELETE FROM `pn_sessions` WHERE `hash` = '{$session_hash}'";
+  $result = mysqli_query($db_connection, $query) or die (mysqli_error($db_connection).$query);
+  session_destroy();
+}
 
-    setcookie('session', $coockie_hash, $session_expires, "/");
+function set_session($user_id) {
+  global $user_agent_hash;
+  global $db_connection;
 
-    $query = "INSERT INTO `pn_sessions` SET `user_id` = '{$user_id}', `hash` = '{$new_session_hash}', `user_agent` = '{$user_agent_hash}', `expires` = '{$session_expires}'";
-    $result = mysqli_query($db_connection, $query) or die (mysqli_error($db_connection).$query);
+  $random_hash = get_hash(rand(0, PHP_INT_MAX), rand(0, PHP_INT_MAX), rand(0, PHP_INT_MAX));
+  $coockie_hash = get_hash($random_hash, $user_id, $user_agent_hash);
+  $session_expires = time() + (60*60*24*30);
+  $new_session_hash = get_hash($coockie_hash, $user_agent_hash, HASH_KEY);
 
-    }
+  setcookie('session', $coockie_hash, $session_expires, "/"); // Создать куку на 30 дней
+
+  $query = "INSERT INTO `pn_sessions` SET `user_id` = '{$user_id}', `hash` = '{$new_session_hash}', `user_agent` = '{$user_agent_hash}', `expires` = '{$session_expires}'";
+  $result = mysqli_query($db_connection, $query) or die (mysqli_error($db_connection).$query); // Создать сессию
+}
 
 $user_agent_hash = md5($_SERVER['HTTP_USER_AGENT']);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $user = new User();
 
-  //   Переводим логин в нижний регистр, потому что пользователь
-  //   может вводить вместо него никнейм, выбранный во время
-  //   регистрации. Если ему так удобнее, переводим никнейм в нижний
-  //   регистр и получаем лоигн.
+  // Переводим логин в нижний регистр, потому что пользователь
+  // может вводить вместо него никнейм, выбранный во время
+  // регистрации. Если ему так удобнее, переводим никнейм в нижний
+  // регистр и получаем логин.
   $received_login = strtolower($_POST['login']);
   $received_login = htmlspecialchars($received_login);
   $received_pass = $_POST['password'];
 
   // Получаем данные юзера из БД
-
   $query = "SELECT * FROM `pn_users` WHERE `login` = '{$received_login}'";
   $result = mysqli_query($db_connection, $query) or die (mysqli_error($db_connection).$query);
-  $user_info = mysqli_fetch_assoc($result);
+  // Записываем данные пользователя в переменную
+  $user_data = mysqli_fetch_assoc($result);
 
-  if (!empty($user_info)) {
-    $ban_severity = (integer)$user_info['ban_severity'];
-    $ban_expires = (integer)$user_info['ban_expires'];
-    $user_id = $user_info['id'];
+  if (!empty($user_data)) {
+    $ban_severity = (integer)$user_data['ban_severity'];
+    $ban_expires = (integer)$user_data['ban_expires'];
+    $user_id = $user_data['id'];
+    $salt = $user_data['salt'];
   }
 
   // Если пользователь с таким логином не найден,
   // возвращаемся к форме входа с сообщением об ошибке.
-    if (empty($user_info)) {
-      header("Location: /login.php?user-does-not-exist=1&&name={$received_login}");
-      exit;
+  if (empty($user_data)) {
+    header("Location: /login.php?user-does-not-exist=1&&name={$received_login}");
+    exit;
   }
 
-  // Если установлен запрет авторизации, он ещё не просочен
+  // Если установлен и ещё не просрочен запрет авторизации,
   // возвращаемся к форме входа с сообщением об ошибке.
-  if (isset($ban_expires) and $ban_expires > time()) {
-      header("Location: /login.php?ban-expires={$ban_expires}&&login={$received_login}");
-      exit;
+  if ($ban_expires > time()) {
+    header("Location: /login.php?ban-expires={$ban_expires}&&login={$received_login}");
+    exit;
   }
 
-  // Следующие шаги определяют идентичность пароля.
-
-   $user->salt = $user_info['salt'];
-
-  if ($user_info['pass'] == get_hash($received_pass, $user->salt, HASH_KEY)) {
-    if ($ban_severity > 0) {
+  if ($user_data['pass'] == get_hash($received_pass, $salt, HASH_KEY)) { // Если пароль прошёл проверку
+    if ($ban_severity > 0) { // Но ранее был установлен запрет авторизации
       $query = "UPDATE `pn_users` SET `ban_expires` = '0', `ban_severity` = 0 WHERE `id` = {$user_id}";
-      mysqli_query($db_connection, $query) or send_error_message(mysqli_error($db_connection).$query);
+      mysqli_query($db_connection, $query) or die (mysqli_error($db_connection).$query); // Сбрасываем его.
     }
-    set_session($user_id);
+
+    set_session($user_id); // Создаём сессию в БД и куку
+    // устанавливаем $_SESSION['user_id'], так как именно отсюда index.php будет
+    // брать информацию о том, вошёл ли кто-то в систему.
     $_SESSION['user_id'] = $user_id;
     header("Location: /");
     exit;
@@ -92,54 +104,46 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if ($ban_severity < 4) {
       $ban_severity++;
-    }
+  }
 
     $severity_rules = [0, 0, 30, 5*60, 30*60]; // Список таймаутов для разных степеней строгости бана (0-4)
     $ban_timeout = $severity_rules[$ban_severity];
     $ban_expires = time() + $ban_timeout;
     $query = "UPDATE `pn_users` SET `ban_expires` = '{$ban_expires}', `ban_severity` = {$ban_severity} WHERE `id` = {$user_id}";
-    mysqli_query($db_connection, $query) or send_error_message(mysqli_error($db_connection).$query);
+    mysqli_query($db_connection, $query) or die (mysqli_error($db_connection).$query);
 
     header("Location: /login.php?ban-expires={$ban_expires}&&login={$received_login}");
     exit;
   }
 
+}
 
-  //- Если запрошен методом get, то есть сессия php удалена
-} else if (isset($_COOKIE['session'])) {
+if ($_SERVER['REQUEST_METHOD'] == 'GET' and isset($_COOKIE['session'])) { // Сработает, если сессия php была окончена
 
-  // Если файл не запрошн методом post, то...
-  //- Проверить легимитность куки
+  // Получаем хэш сессии, соответствующий хэшу куки
   $cookies_session_hash = get_hash($_COOKIE['session'], $user_agent_hash, HASH_KEY);
+  // Ищем эту сессию в БД
   $query = "SELECT * FROM `pn_sessions` WHERE `hash` = '{$cookies_session_hash}'";
   $result = mysqli_query($db_connection, $query) or die (mysqli_error($db_connection).$query);
+  // Записываем данные о найденной сессии в переменную
   $session = mysqli_fetch_assoc($result);
 
-  // Если кука не просрочена и браузер совпадает с записанным в БД,
-  if ((integer)$session['expires'] > time() and $session['user_agent'] === $user_agent_hash) {
+  // Если браузер совпадает с записанным в сессии,
+  if ($session['user_agent'] === $user_agent_hash) {
 
-    // ЗАМЕНИТЬ КУКУ И СЕССИЮ
-    setcookie('session', '', time());
-    $query = "DELETE FROM `pn_sessions` WHERE `hash` = '{$cookies_session_hash}'";
-    $result = mysqli_query($db_connection, $query) or die (mysqli_error($db_connection).$query);
-    session_destroy();
-
+    // Заменить куку и сессию
+    remove_session($cookies_session_hash);
     set_session($session['user_id']);
 
     return $session['user_id'];
-  } else {
-    // или удалить куку и сессию
-    setcookie('session', '', time());
-    $query = "DELETE FROM `pn_sessions` WHERE `hash` = '{$cookies_session_hash}'";
-    $result = mysqli_query($db_connection, $query) or die (mysqli_error($db_connection).$query);
-    session_destroy();
+  } else if (isset($_COOKIE['session'])) { // Если сессия из куки есть, но браузер не совпадает
+    // удалить куку и сессию
+    remove_session($cookies_session_hash);
+
     header("Location: /login.php");
     exit;
   }
 } else {
-      header("Location: /login.php");
-      exit;
+    header("Location: /login.php");
+    exit;
 }
-
-
-// Файл возвращает стр()"2", хотя сессия удалена
--- login.php
+++ login.php
@@ -1,37 +1,27 @@
 <?php
 
-   /*
-    * Форма регистрации пользователя.
-    *
-    * Переменные:
-    *
-    * $error_message: Сообщение об ошибке регистрации (если она есть).
-    * $name: имя пользователя, если оно было введено до возникновения ошибки
-    *   (например, если неправильно введена капча).
-    * $email e-mail пользователя, если оно было введено до возникновения ошибки
-    * $captcha->html_content: html-код блока капчи.
-    *
-    */
+/*
+* /login.php
+*
+* Форма входа на сайт.
+*
+*/
 
-  ini_set('error_reporting', E_ALL);
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 
-//   include_once "{$_SERVER['DOCUMENT_ROOT']}/includes/classes/User.php";
+$title = "Вход";
 
-  $title = "Вход";
+$error_message = "";
+$login = "";
 
-
-  $error_message = ""; // Вынести в auth.php (???)
-  $login = "";
-
-  if (isset($_GET['user-does-not-exist'])) {
-    $error_message = "Пользователь {$_GET['name']} не найден. Вы можете <a href=\"/registration-form.php?name={$_GET['name']}\">зарегистрироваться</a> с таким именем.";
-  } else if (isset($_GET['ban-expires'])) {
-    $error_message = "Неверный пароль. Попробуйте ещё раз через <span id=\"timer\"></span>.";
-    $ban_timeout = $_GET['ban-expires'] - time();
-  }
-
+if (isset($_GET['user-does-not-exist'])) {
+  $error_message = "Пользователь {$_GET['name']} не найден. Вы можете <a href=\"/registration-form.php?name={$_GET['name']}\">зарегистрироваться</a> с таким именем.";
+} else if (isset($_GET['ban-expires'])) {
+  $error_message = "Неверный пароль. Попробуйте ещё раз через <span id=\"timer\"></span>.";
+  $ban_timeout = $_GET['ban-expires'] - time();
+}
 
 ?>
 <!DOCTYPE html>
@@ -41,72 +31,6 @@
     <title><?= $title ?></title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="shortcut icon" href="/images/icons/favicon.ico">
-    <style>
-
-      #login-form {
-        width: 200px;
-        margin: 0 auto;
-      }
-
-      #login-form label,
-      #login-form input,
-      #login-form button {
-        display: block;
-      }
-
-      #login-form input[type="text"],
-      #login-form input[type="password"] {
-        width: 200px;
-        padding: 2px;
-      }
-
-      .centered {
-        text-align: center;
-      }
-
-      h1.centered {
-        margin: 12px 0;
-      }
-
-      #login-form label {
-        margin: 2px;
-        padding: 12px 0px;
-      }
-
-      #login-form button {
-        margin: 25px auto;
-      }
-
-
-     main.login-form-container {
-       margin: 0 auto;
-       height: 100vh;
-       display: grid;
-       justify-content: center;
-       align-items: center;
-     }
-
-    div.login-form-container {
-      margin: 0 auto calc(50vh - 160px) auto;
-      padding: 10px 85px;
-      width: 372px;
-      border: 1px solid #abbdc4;
-      background: #c3cfd5;
-      border-radius: 5px;
-    }
-
-    div#error_message {
-      align-self: end;
-      padding: 0px 25px;
-      width: 65vw;
-      height: 70px;
-      border: 1px solid #999;
-      background: #fdd;
-      line-height: 65px;
-      text-align: center;
-    }
-
-    </style>
   </head>
   <body>
     <main  class="login-form-container">
@@ -127,28 +51,18 @@
     </div>
     </main>
   </body>
-
-
   <script>
-
   <?php if (empty($error_message)): ?>
     let errorMessage = document.getElementById('error_message');
-    errorMessage.style.opacity = "0";
-
+    errorMessage.style.opacity = "0"; // Делаем пустой блок вывода ошибок невидимым.
   <?php endif; ?>
-
-
-  <?php
-  if (isset($ban_timeout)):
-  ?>
+  <?php if (isset($ban_timeout)): ?>// Если из адресгой строки получено значение таймаута бана
 
     let timer = document.getElementById('timer');
-    // получить задержку
+    // передать величину задержки в JS
     let timeout = <?= $ban_timeout ?>;
-    // усли таймаут не вышел, заблокировать ввод
+    // усли таймаут не окончен, заблокировать ввод и вывести сообщение с обратным отсчётом
     if (timeout > 0) {
-      //  УСТАНОВИТЬ ТАЙМЕР
-      // ЗАБЛОКИРОВАТЬ ВВОД
       function clock() {
         let submit = document.getElementById('submit');
         submit.setAttribute("disabled", "disabled");
@@ -173,15 +87,10 @@
            errorMessage.style.opacity = "0";
         }
         timeout--;
-
       }
 
       let timeCounter = setInterval(clock, 1000);
     }
-    // создать таймер
-
   <?php endif; ?>
   </script>
-
-
 </html>
