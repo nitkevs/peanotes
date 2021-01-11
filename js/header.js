ca88eb1a21e96c5d2d5d7c959fe32678b601cfd7
nikevs <nitkevs@gmail.com>
2021-01-11 23:01:38 +0200

    Исправлена ошибка в js/header.js
    Если пользователь неавторизован, скрипт всё равно пытается получить
    доступ к меню пользователя, которого нет. В консоли высыпает ошибку.
    Исправлено.

--- js/header.js
+++ js/header.js
@@ -1,17 +1,18 @@
 let menu = document.getElementById('user-menu');
-menu.container = document.getElementById('header-user-menu');
-menu.state = 'closed';
-menu.toggle = function() {
-  if (menu.state === 'closed') {
-    menu.container.style.transitionDelay = '0s';
-    menu.container.style.background = '#657e8b';
-    menu.style.height = '94px';
-    menu.state = 'opened';
-  } else {
-    menu.container.style.transitionDelay = '0.2s';
-    menu.container.style.background = 'transparent';
-    menu.style.height = '0px';
-    menu.state = 'closed';
+if (menu) {
+  menu.container = document.getElementById('header-user-menu');
+  menu.state = 'closed';
+  menu.toggle = function() {
+    if (menu.state === 'closed') {
+      menu.container.style.transitionDelay = '0s';
+      menu.container.style.background = '#657e8b';
+      menu.style.height = '94px';
+      menu.state = 'opened';
+    } else {
+      menu.container.style.transitionDelay = '0.2s';
+      menu.container.style.background = 'transparent';
+      menu.style.height = '0px';
+      menu.state = 'closed';
+    }
   }
 }
-
