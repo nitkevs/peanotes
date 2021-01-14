72c63207043e713beea06427a65f41a8c7088cce
nikevs <nitkevs@gmail.com>
2021-01-14 21:36:59 +0200

    Изменена анимация меню пользователя.

--- js/header.js
+++ js/header.js
@@ -4,15 +4,18 @@ if (menu) {
   menu.state = 'closed';
   menu.toggle = function() {
     if (menu.state === 'closed') {
+      menu.container.style.transitionDuration = '0s';
       menu.container.style.transitionDelay = '0s';
       menu.container.style.background = '#657e8b';
       menu.style.height = '94px';
       menu.state = 'opened';
     } else {
-      menu.container.style.transitionDelay = '0.2s';
+      menu.container.style.transitionDuration = '0.15s';
+      menu.container.style.transitionDelay = '0.1s';
       menu.container.style.background = 'transparent';
       menu.style.height = '0px';
       menu.state = 'closed';
     }
   }
 }
+
